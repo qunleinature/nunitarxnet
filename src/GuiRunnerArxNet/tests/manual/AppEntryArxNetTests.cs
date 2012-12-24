c4093161d3681ed709cfd49b046a1846dc45bfa1
qunleinature <leiqun.nature.mail@gmail.com>
2012-12-24 17:41:19 +0800

    2012-12-24 17:40 添加测试

---
+++ src/GuiRunnerArxNet/tests/manual/AppEntryArxNetTests.cs
@@ -0,0 +1,29 @@
+﻿// ****************************************************************
+//2012年12月24日，雷群修改:
+//  1.添加测试项目：nunit-gui-arxnet.tests.manua（需人工交互）
+//  2.测试类：AppEntryArxNetTests
+// ****************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+using NUnit.Gui.ArxNet;
+
+namespace NUnit.Gui.ArxNet.Tests
+{
+    [TestFixture]
+    public class AppEntryArxNetTests
+    {
+        //public int Main(string[] args)
+        [Test]
+        public void Main()
+        {
+            string[] args = new string[0];
+            AppEntryArxNet appEntryArxNet = new AppEntryArxNet();
+            appEntryArxNet.Main(args);
+        }
+    }
+}
