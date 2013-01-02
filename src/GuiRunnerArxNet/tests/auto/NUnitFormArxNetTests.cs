09a90e60e83603610a74fafb1c11de10ed5edf40
qunleinature <leiqun.nature.mail@gmail.com>
2013-01-02 10:40:31 +0800

    2013-1-2 10:39 单元测试​NUnit.Gui.ArxNet.Tests.NUnitFormArxNetTests.Constructor

---
+++ src/GuiRunnerArxNet/tests/auto/NUnitFormArxNetTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+using NUnit.Util;
+using NUnit.Util.ArxNet;
+
+using Com.Utility.UnitTest;
+
+namespace NUnit.Gui.ArxNet.Tests
+{
+    public class NUnitFormArxNetTests
+    {
+        //Construction
+		//public NUnitFormArxNet( GuiOptions guiOptions ) : base("NUnit")
+        [Test]
+        public void Constructor()
+        {
+            GuiOptions expectedGuiOptions = new GuiOptions(new string[0]);
+            NUnitFormArxNet nUnitFormArxNet = new NUnitFormArxNet(expectedGuiOptions);
+            Assert.That(nUnitFormArxNet, Is.Not.Null);
+            //private GuiOptions guiOptions;
+            GuiOptions actualGuiOptions = UnitTestHelper.GetNonPublicField(nUnitFormArxNet, "guiOptions") as GuiOptions;
+            Assert.That(actualGuiOptions, Is.EqualTo(expectedGuiOptions));
+            //private RecentFiles recentFilesService;
+            RecentFiles expectedRecentFilesService = ServicesArxNet.RecentFiles;
+            RecentFiles actualRecentFilesService = UnitTestHelper.GetNonPublicField(nUnitFormArxNet, "recentFilesService") as RecentFiles;
+            Assert.That(actualRecentFilesService, Is.EqualTo(expectedRecentFilesService));
+            //private ISettings userSettings;
+            ISettings expectedUserSettings = ServicesArxNet.UserSettings;
+            ISettings actualUserSettings = UnitTestHelper.GetNonPublicField(nUnitFormArxNet, "userSettings") as ISettings;
+            Assert.That(actualUserSettings, Is.EqualTo(expectedUserSettings));
+            //private NUnitPresenterArxNet presenter = null;
+            NUnitPresenterArxNet actualPresenter = UnitTestHelper.GetNonPublicField(nUnitFormArxNet, "presenter") as NUnitPresenterArxNet;
+            Assert.That(actualPresenter.Form, Is.EqualTo(nUnitFormArxNet));
+            TestLoaderArxNet actualLoader = UnitTestHelper.GetNonPublicField(actualPresenter, "loader") as TestLoaderArxNet;
+            //private TestLoaderArxNet TestLoader
+            TestLoaderArxNet expectedLoader = UnitTestHelper.GetNonPublicProperty(nUnitFormArxNet, "TestLoader") as TestLoaderArxNet;
+            Assert.That(actualLoader, Is.EqualTo(expectedLoader));
+        }
+    }
+}
---
+++ src/GuiRunnerArxNet/tests/manual/NUnitFormArxNetTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+using NUnit.Util;
+using NUnit.Util.ArxNet;
+
+using Com.Utility.UnitTest;
+
+namespace NUnit.Gui.ArxNet.Tests
+{
+    public class NUnitFormArxNetTests
+    {
+        
+    }
+}
