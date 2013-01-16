5e26c3964c4f3b43a54060f4ff4a78d5e90d1dfe
qunleinature <leiqun.nature.mail@gmail.com>
2013-01-16 11:34:39 +0800

    2013-1-16 11:34 NUnit.Gui.ArxNet.Tests.OptionsDialogArxNetTests.Display测试用例

---
+++ src/GuiRunnerArxNet/tests/manual/OptionsDialogArxNetTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using NUnit.Framework;
+using NUnit.Util;
+using NUnit.Util.ArxNet;
+using NUnit.UiKit;
+using NUnit.UiKit.ArxNet;
+
+namespace NUnit.Gui.ArxNet.Tests
+{
+    [TestFixture]
+    public class OptionsDialogArxNetTests
+    {
+        //public static void Display( Form owner )
+        [Test]
+        public void Display()
+        {
+            SettingsServiceArxNet settingsService = new SettingsServiceArxNet();
+            ServiceManager.Services.AddService(settingsService);
+            ServiceManager.Services.AddService(new DomainManager());
+            ServiceManager.Services.AddService(new RecentFilesService());
+            ServiceManager.Services.AddService(new ProjectService());
+            ServiceManager.Services.AddService(new TestLoaderArxNet(new GuiTestEventDispatcherArxNet()));
+            ServiceManager.Services.AddService(new AddinRegistry());
+            ServiceManager.Services.AddService(new AddinManager());
+            ServiceManager.Services.AddService(new TestAgency());
+            ServiceManager.Services.InitializeServices();
+            AppContainer c = new AppContainer();
+            AmbientProperties ambient = new AmbientProperties();
+            c.Services.AddService(typeof(AmbientProperties), ambient);
+            GuiOptions guiOptions = new GuiOptions(new string[0]);
+            NUnitFormArxNet owner = new NUnitFormArxNet(guiOptions);
+            c.Add(owner);
+            OptionsDialogArxNet.Display(owner);
+        }
+    }
+}
