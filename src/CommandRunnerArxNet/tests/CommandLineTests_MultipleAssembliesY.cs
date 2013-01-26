2ca9e745e84c7f27c17a50e8d030f227dfd3b0ea
qunleinature <leiqun.nature.mail@gmail.com>
2013-01-26 21:15:38 +0800

    2013-1-26 21:15 测试NUnit.CommandRunner.ArxNet.Tests.CommandLineTests_MultipleAssemblies

--- src/CommandRunnerArxNet/tests/CommandLineTests_MultipleAssembliesY.cs
+++ src/CommandRunnerArxNet/tests/CommandLineTests_MultipleAssembliesY.cs
@@ -5,36 +5,34 @@
 // ****************************************************************
 
 // ****************************************************************
-// Copyright 2013, Lei Qun
-// 2013.1.26：
-//  在NUnit2.6.2基础上修改
+//2012年1月16日，雷群修改
 // ****************************************************************
 
 namespace NUnit.CommandRunner.ArxNet.Tests
 {
-    using System;
-    using System.Collections;
+	using System;
+	using System.Collections;
     using NUnit.Core;
-    using NUnit.Framework;
+	using NUnit.Framework;
 
-    using NUnit.CommandRunner.ArxNet;//2013.1.26改
+    using NUnit.CommandRunner.ArxNet;
 
 	[TestFixture]
 	public class CommandLineTests_MultipleAssemblies
 	{
-        private readonly string firstAssembly = "nunit.tests.dll";
-        private readonly string secondAssembly = "mock-assembly.dll";
-        private readonly string fixture = "NUnit.Tests.CommandLine";
-        private CommandOptionsArxNet assemblyOptions;//2013.1.26改
-        private CommandOptionsArxNet fixtureOptions;//2013.1.26改
+		private readonly string firstAssembly = "nunit.tests.dll";
+		private readonly string secondAssembly = "mock-assembly.dll";
+		private readonly string fixture = "NUnit.Tests.CommandLine";
+		private CommandOptionsArxNet assemblyOptions;
+		private CommandOptionsArxNet fixtureOptions;
 
 		[SetUp]
 		public void SetUp()
 		{
-            assemblyOptions = new CommandOptionsArxNet(new string[] 
-                { firstAssembly, secondAssembly });//2013.1.26改
-            fixtureOptions = new CommandOptionsArxNet(new string[] 
-                { "-fixture:" + fixture, firstAssembly, secondAssembly });//2013.1.26改
+			assemblyOptions = new CommandOptionsArxNet(new string[]
+				{ firstAssembly, secondAssembly });
+			fixtureOptions = new CommandOptionsArxNet(new string[]
+				{ "-fixture:"+fixture, firstAssembly, secondAssembly });
 		}
 
 		[Test]
