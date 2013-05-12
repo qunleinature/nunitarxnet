168c092d8d51753be91f465d3afdbf5ca57aa6cb
qunleinature <88995031@qq.com>
2013-05-12 20:48:52 +0800

    2013-5-12 20:48

--- src/NUnitCoreArxNet/nunit-core-arxnet-dll/EditorWriter.cs
+++ src/NUnitCoreArxNet/nunit-core-arxnet-dll/EditorWriter.cs
@@ -14,12 +14,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-using System.Text;
+using System.Runtime.InteropServices;
 
 namespace NUnit.Core.ArxNet
 {
+    [Serializable, ComVisible(true)]
     public class EditorWriter : TextWriter
     {
 
+        public override Encoding Encoding
+        {
+            get { throw new NotImplementedException(); }
+        }
     }
 }
