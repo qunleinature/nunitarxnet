cb10786aa00d0103debf66581809a1f301a208b7
qunleinature <88995031@qq.com>
2013-05-15 21:01:48 +0800

    2013-5-15 21:01

--- src/NUnitCoreArxNet/nunit-core-arxnet-dll/EditorWriter.cs
+++ src/NUnitCoreArxNet/nunit-core-arxnet-dll/EditorWriter.cs
@@ -16,23 +16,54 @@ using System.Text;
 using System.IO;
 using System.Runtime.InteropServices;
 
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.ApplicationServices;
+
 namespace NUnit.Core.ArxNet
 {
     [Serializable, ComVisible(true)]
     public class EditorWriter : TextWriter
     {
-        private static UnicodeEncoding m_encoding;
+        private static UnicodeEncoding m_Encoding;
+        private Editor m_Editor;
+
+        public EditorWriter()
+        {
+            //throw new System.NotImplementedException();
+            m_Encoding = new UnicodeEncoding(false, false);
+            m_Editor = Application.DocumentManager.MdiActiveDocument.Editor;
+        }
+
+        public EditorWriter(Editor ed)
+        {
+            //throw new System.NotImplementedException();
+            m_Encoding = new UnicodeEncoding(false, false);
+            m_Editor = ed;
+        }
 
         public override Encoding Encoding
         {
             get 
             { 
                 //throw new NotImplementedException(); 
-                if (m_encoding == null)
+                if (m_Encoding == null)
                 {
-                    m_encoding = new UnicodeEncoding(false, false);
+                    m_Encoding = new UnicodeEncoding(false, false);
                 }
-                return m_encoding;
+                return m_Encoding;
+            }
+        }
+
+        public Editor Editor
+        {
+            get
+            {
+                //throw new System.NotImplementedException();
+                if (m_Editor == null)
+                {
+                    m_Editor = Application.DocumentManager.MdiActiveDocument.Editor;
+                }
+                return m_Editor;
             }
         }
     }
--- src/NUnitCoreArxNet/tests/auto/EditorWriterTests.cs
+++ src/NUnitCoreArxNet/tests/auto/EditorWriterTests.cs
@@ -14,6 +14,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.ApplicationServices;
+
 using NUnit.Framework;
 
 namespace NUnit.Core.ArxNet.Tests
@@ -21,12 +24,64 @@ namespace NUnit.Core.ArxNet.Tests
     [TestFixture]
     public class EditorWriterTests
     {
+        //public EditorWriter()
+        [Test]
+        public void Constructor_Default()
+        {
+            EditorWriter editorWriter = new EditorWriter();
+            Assert.That(editorWriter, Is.Not.Null);
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            Assert.That(editorWriter.Editor, Is.EqualTo(ed));
+            UnicodeEncoding encoding = new UnicodeEncoding(false, false);
+            Assert.That(editorWriter.Encoding.CodePage, Is.EqualTo(encoding.CodePage));
+        }
+
+        //public EditorWriter(Editor ed)
+        [Test]
+        [Category("Constructor_Editor")]
+        public void Constructor_EditorIsActive()
+        {
+            Editor ed= Application.DocumentManager.MdiActiveDocument.Editor;
+            EditorWriter editorWriter = new EditorWriter(ed);
+            Assert.That(editorWriter, Is.Not.Null);
+            Assert.That(editorWriter.Editor, Is.EqualTo(ed));
+            UnicodeEncoding encoding = new UnicodeEncoding(false, false);
+            Assert.That(editorWriter.Encoding.CodePage, Is.EqualTo(encoding.CodePage));
+        }
+
+        [Test]
+        [Category("Constructor_Editor")]
+        public void Constructor_EditorNotActive()
+        {
+            Editor ed = Application.DocumentManager.Add(null).Editor;            
+            EditorWriter editorWriter = new EditorWriter(ed);
+            Assert.That(editorWriter, Is.Not.Null);
+            ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            Assert.That(editorWriter.Editor, Is.EqualTo(ed));
+            UnicodeEncoding encoding = new UnicodeEncoding(false, false);
+            Assert.That(editorWriter.Encoding.CodePage, Is.EqualTo(encoding.CodePage));
+        }
+
+        //public Editor Editor
+        //public override Encoding Encoding
+        [Test]
+        public void Get_Editor()
+        {
+            EditorWriter editorWriter = new EditorWriter();
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            Assert.That(editorWriter.Editor, Is.Not.Null);
+            Assert.That(editorWriter.Editor, Is.EqualTo(ed));
+        }
+
         //public override Encoding Encoding
         [Test]
         public void Get_Encoding()
         {
             EditorWriter editorWriter = new EditorWriter();
+            UnicodeEncoding encoding = new UnicodeEncoding(false, false);
             Assert.That(editorWriter.Encoding, Is.Not.Null);
+            Assert.That(editorWriter.Encoding.CodePage, Is.EqualTo(encoding.CodePage));
+
         }
     }
 }
