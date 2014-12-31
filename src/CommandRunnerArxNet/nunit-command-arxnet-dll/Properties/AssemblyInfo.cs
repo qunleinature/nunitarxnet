a11d165dfa94c3ce217307b299ae418f19efeacc
qunleinature <88995031@qq.com>
2014-12-31 23:34:41 +0800

    2014.12.31 23:34

--- src/CommandRunnerArxNet/nunit-command-arxnet-dll/Properties/AssemblyInfo.cs
+++ src/CommandRunnerArxNet/nunit-command-arxnet-dll/Properties/AssemblyInfo.cs
@@ -25,5 +25,6 @@ using System.Runtime.InteropServices;
 //      修正版本号
 //      编译版本号
 //
-[assembly: AssemblyVersion("0.0.5.0")]
-[assembly: AssemblyFileVersionAttribute("0.0.5.0")]
+[assembly: AssemblyVersion("0.0.6.0")]
+[assembly: AssemblyFileVersionAttribute("0.0.6.0")]
+[assembly: AssemblyTitleAttribute("Nunit-Command-ArxNet")]
